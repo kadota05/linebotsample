cc41199154e57c67522125882b0a66cb82c53d3e
kadota05 <mastnoclass@gmail.com>
2023-11-22 13:40:15 +0900

    メインアプリを追加したぜ！

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+app = Flask(__name__)
+
+CHANNEL_ACCESS_TOKEN = 'your_channel_access_token'
+CHANNEL_SECRET = 'your_channel_secret'
+
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))  # オウム返し
+
+if __name__ == "__main__":
+    app.run()
